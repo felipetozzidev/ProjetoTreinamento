d54ceed9d946eb96d7954127f7ddb64c556a6125
felipetozzi_dev <fetobertochi@gmail.com>
2026-02-21 22:52:18 -0300

    Correção do problema de captura dos dados passados para o front end e adição dos itens de forma adequada

--- projetotreinamento.client/src/app/components/bodies/tarefas-body/tarefas-body.component.ts
+++ projetotreinamento.client/src/app/components/bodies/tarefas-body/tarefas-body.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewChildren, QueryList, ElementRef, AfterViewInit } from '@angular/core';
 import { ChecklistType } from '../../../types/ChecklistType';
 import { TarefaService } from '../../../services/tarefas/tarefa.service';
 import { ChecklistService } from '../../../services/checklists/checklist.service';
@@ -10,41 +10,60 @@ import { ItemType } from '../../../types/ItemType';
   templateUrl: './tarefas-body.component.html',
   styleUrl: './tarefas-body.component.css'
 })
-export class TarefasBodyComponent {
+export class TarefasBodyComponent implements AfterViewInit {
   @Input({ required: true })
   idTarefa: number = 0;
 
-  idChecklist: number = 1;
+  idChecklist: number = 0;
 
 
   checklists: ChecklistType[] = [
 
   ];
 
-  items: ItemType[] = [
-
-  ];
-
-  
+  itemsPorChecklist: Record<number, ItemType[]> = {};
 
   constructor(private tarefaService: TarefaService, private checklistService: ChecklistService) { }
 
+  @ViewChildren('card') cards!: QueryList<ElementRef>;
+
+  ngAfterViewInit(): void {
+    this.cards.changes.subscribe(() => {
+      const listaCards = this.cards.toArray();
+      console.log('Cards renderizados:', listaCards);
+
+      listaCards.forEach((card, index) => {
+        const element = card.nativeElement;
+        console.log(`Card ${index}:`, element.id, element.textContent);
+        this.carregarItensPorChecklist;
+      });
+    });
+  }
+
   ngOnInit(): void {
     this.carregarChecklists();
-    this.carregarItens()
+
   }
 
   carregarChecklists() {
     this.tarefaService.selectTarefaChildren(this.idTarefa).subscribe({
-      next: (dados) => { console.log(dados); this.checklists.push(...dados.retorno) },
-      error: (err) => console.error('Erro ao carregar tarefas', err)
+      next: (dados) => {
+        this.checklists = dados.retorno;
+
+        // Agora carrega os itens do checklist de forma individual
+        this.checklists.forEach(ch => {
+          this.carregarItensPorChecklist(ch.id);
+        });
+      },
     });
   }
 
-  carregarItens() {
-    this.checklistService.listItemsChecklist(this.idChecklist).subscribe({
-      next: (dados) => { console.log(dados); this.items.push(...dados.retorno) },
-      error: (err) => console.error('Erro ao carregar itens', err)
+  carregarItensPorChecklist(idChecklist: number) {
+    this.checklistService.listItemsChecklist(idChecklist).subscribe({
+      next: (dados) => {
+        this.itemsPorChecklist[idChecklist] = dados.retorno;
+      },
+      error: err => console.error('Erro ao carregar itens', err)
     });
   }
 
